fa833c3de9e8777d27643349215eaf32943875c3
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-09-30 16:21:32 +0700

    Update sql

--- db_sppd.sql
+++ db_sppd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 29, 2017 at 01:40 AM
+-- Generation Time: Sep 30, 2017 at 09:21 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.30
 
@@ -99,7 +99,12 @@ INSERT INTO `city` (`id`, `name`, `province_id`) VALUES
 (11, 'Kendal', 4),
 (19, 'Purbalingga', 4),
 (20, 'Godong', 4),
-(21, 'Purwokerto', 4);
+(21, 'Purwokerto', 4),
+(22, 'Jakarta Utara', 6),
+(23, 'Jakarta Selatan', 6),
+(24, 'Jakarta Tengah', 6),
+(25, 'Malang', 5),
+(26, 'Sukabumi', 2);
 
 -- --------------------------------------------------------
 
@@ -110,8 +115,8 @@ INSERT INTO `city` (`id`, `name`, `province_id`) VALUES
 CREATE TABLE `officer` (
   `id` int(10) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `officer_id` varchar(100) NOT NULL,
   `phone_number` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
   `office_class_id` int(11) DEFAULT NULL,
   `office_position_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -120,9 +125,11 @@ CREATE TABLE `officer` (
 -- Dumping data for table `officer`
 --
 
-INSERT INTO `officer` (`id`, `name`, `phone_number`, `email`, `office_class_id`, `office_position_id`) VALUES
-(1, 'Ir. Setiyono, M.Si', '081123456', 'setiyono@email.com', 6, 1),
-(2, 'Nama', 'telp', 'email', 4, 4);
+INSERT INTO `officer` (`id`, `name`, `officer_id`, `phone_number`, `office_class_id`, `office_position_id`) VALUES
+(1, 'Ir. Setiyono, M.Si', '081123456', '081111133434343', 6, 1),
+(2, 'Pegawai 1', '43234234', '024234234', 4, 4),
+(3, 'Pegawai 2', '43234234', '024234234', 5, 3),
+(4, 'Pegawai 3', '43234234', '024234234', 6, 4);
 
 -- --------------------------------------------------------
 
@@ -185,10 +192,69 @@ CREATE TABLE `province` (
 --
 
 INSERT INTO `province` (`id`, `name`) VALUES
-(6, 'Dki Jakarta'),
+(6, 'DKI Jakarta'),
 (2, 'Jawa Barat'),
 (4, 'Jawa Tengah'),
 (5, 'Jawa Timur');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sppd`
+--
+
+CREATE TABLE `sppd` (
+  `id` int(11) NOT NULL,
+  `state` int(1) NOT NULL DEFAULT '1',
+  `reference_number` varchar(100) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `total_day` int(11) DEFAULT NULL,
+  `base` text NOT NULL,
+  `objective` text NOT NULL,
+  `task` text NOT NULL,
+  `description` text NOT NULL,
+  `chief_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sppd`
+--
+
+INSERT INTO `sppd` (`id`, `state`, `reference_number`, `start_date`, `end_date`, `total_day`, `base`, `objective`, `task`, `description`, `chief_name`) VALUES
+(5, 1, '3.a/BTPAL/ST/10/2017', '2017-09-01', '2017-09-03', 3, 'dasar', 'Brebes', 'tugas', 'keterangan', 'Ir. Setiyono, M.Si'),
+(6, 1, '2.a/BTPAL/ST/10/2017', '2017-09-01', '2017-09-04', 4, 'Dasar1', 'Cirebon', 'Tigas', 'Keterangan', 'Ir. Setiyono, M.Si'),
+(7, 1, '7.a/BTPAL/ST/10/2017', '2017-09-01', '2017-09-05', 5, 'd', 'Brebes', 'asdf', 'safd', 'Ir. Setiyono, M.Si'),
+(8, 1, '8.a/BTPAL/ST/10/2017', '2017-09-08', '2017-09-16', 8, 'asfdasdf', 'Cirebon', 'sadf', 'asdf', 'Ir. Setiyono, M.Si');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sppd_officer`
+--
+
+CREATE TABLE `sppd_officer` (
+  `id` int(11) NOT NULL,
+  `sppd_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `officer_id` varchar(100) NOT NULL,
+  `office_class_name` varchar(100) NOT NULL,
+  `office_position_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sppd_officer`
+--
+
+INSERT INTO `sppd_officer` (`id`, `sppd_id`, `name`, `officer_id`, `office_class_name`, `office_position_name`) VALUES
+(6, 5, 'Pegawai 1', '43234234', '2B', 'Staff'),
+(7, 5, 'Pegawai 3', '43234234', '2D', 'Staff'),
+(13, 6, 'Ir. Setiyono, M.Si', '081123456', '2D', 'Kepala Balai Teknologi'),
+(14, 7, 'Pegawai 2', '43234234', '2C', 'Bendahara'),
+(15, 7, 'Pegawai 1', '43234234', '2B', 'Staff'),
+(16, 7, 'Pegawai 3', '43234234', '2D', 'Staff'),
+(17, 8, 'Ir. Setiyono, M.Si', '081123456', '2D', 'Kepala Balai Teknologi'),
+(18, 8, 'Pegawai 2', '43234234', '2C', 'Bendahara');
 
 -- --------------------------------------------------------
 
@@ -234,7 +300,29 @@ INSERT INTO `travel_expenses` (`id`, `office_class_id`, `province_id`, `transpor
 (2, 1, 2, 430123, 435000),
 (4, 1, 2, 430000, 435000),
 (5, 1, 2, 430000, 435000),
-(6, 1, 6, 2000, 2000);
+(6, 1, 6, 300000, 330000),
+(7, 2, 6, 100000, 100000),
+(8, 3, 6, 100000, 100000),
+(9, 4, 6, 100000, 100000),
+(10, 5, 6, 100000, 100000),
+(11, 6, 6, 100000, 100000),
+(12, 2, 2, 100000, 100000),
+(13, 3, 2, 100000, 100000),
+(14, 4, 2, 100000, 100000),
+(15, 5, 2, 100000, 100000),
+(16, 6, 2, 100000, 100000),
+(17, 1, 4, 100000, 100000),
+(18, 2, 4, 100000, 100000),
+(19, 3, 4, 100000, 100000),
+(20, 4, 4, 100000, 100000),
+(21, 5, 4, 100000, 100000),
+(22, 6, 4, 100000, 100000),
+(23, 1, 5, 100000, 100000),
+(24, 2, 5, 100000, 100000),
+(25, 3, 5, 100000, 100000),
+(26, 4, 5, 100000, 100000),
+(27, 5, 5, 100000, 100000),
+(28, 6, 5, 100000, 100000);
 
 --
 -- Indexes for dumped tables
@@ -290,6 +378,19 @@ ALTER TABLE `province`
   ADD UNIQUE KEY `name` (`name`);
 
 --
+-- Indexes for table `sppd`
+--
+ALTER TABLE `sppd`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sppd_officer`
+--
+ALTER TABLE `sppd_officer`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sppd_id` (`sppd_id`);
+
+--
 -- Indexes for table `transport`
 --
 ALTER TABLE `transport`
@@ -321,17 +422,17 @@ ALTER TABLE `budget`
 -- AUTO_INCREMENT for table `city`
 --
 ALTER TABLE `city`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `officer`
 --
 ALTER TABLE `officer`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `office_class`
 --
 ALTER TABLE `office_class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `office_position`
 --
@@ -343,6 +444,16 @@ ALTER TABLE `office_position`
 ALTER TABLE `province`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
+-- AUTO_INCREMENT for table `sppd`
+--
+ALTER TABLE `sppd`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `sppd_officer`
+--
+ALTER TABLE `sppd_officer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
 -- AUTO_INCREMENT for table `transport`
 --
 ALTER TABLE `transport`
@@ -351,7 +462,7 @@ ALTER TABLE `transport`
 -- AUTO_INCREMENT for table `travel_expenses`
 --
 ALTER TABLE `travel_expenses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
 -- Constraints for dumped tables
 --
@@ -368,6 +479,12 @@ ALTER TABLE `city`
 ALTER TABLE `officer`
   ADD CONSTRAINT `officer_ibfk_1` FOREIGN KEY (`office_position_id`) REFERENCES `office_position` (`id`) ON DELETE SET NULL,
   ADD CONSTRAINT `officer_ibfk_2` FOREIGN KEY (`office_class_id`) REFERENCES `office_class` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `sppd_officer`
+--
+ALTER TABLE `sppd_officer`
+  ADD CONSTRAINT `sppd_officer_ibfk_1` FOREIGN KEY (`sppd_id`) REFERENCES `sppd` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `travel_expenses`
