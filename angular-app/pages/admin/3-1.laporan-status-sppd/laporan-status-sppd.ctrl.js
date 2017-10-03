e299bbc48924c13f7d51af8348870ab90c78d6ea
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-10-03 09:06:22 +0700

    Show completed sppd

--- angular-app/pages/admin/3-1.laporan-status-sppd/laporan-status-sppd.ctrl.js
+++ angular-app/pages/admin/3-1.laporan-status-sppd/laporan-status-sppd.ctrl.js
@@ -12,18 +12,21 @@
 
 		vm.labels = ['Input Surat Tugas', 'Input SPD', 'Input Biaya Dinas', 'Input Bukti Kas', 'Input Hasil Dinas'];
 		vm.data = [0, 0, 0, 0, 0];
+		vm.completed = 0;
 
 		abstractPage.startCtrl().then(activate);
 		function activate() {
 			vm.search = '';
 
-			var promises = [ dataservice.getAllSPPD() ];
+			var promises = [ dataservice.getAllSPPD(), dataservice.getAllSPPDState6() ];
 			$q.all( promises ).then( afterGetAll );
 			function afterGetAll( responses ) {
 				var data = responses[0];
 				_.forEach(data, function( d ) {
 					vm.data[ d.state - 1 ]++;
 				});
+
+				vm.completed = responses[1].length;
 			}
 		}
 	}
