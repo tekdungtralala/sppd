2378dd6dc49f02a63a8ff549b7d8a90a9ca82f8d
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-11-15 00:52:50 +0700

    Print surat tugas

---
+++ api/print/2-surat-tugas.php
@@ -0,0 +1,22 @@
+<?php
+	include '../connect.php';
+	session_start();
+	$data = $_SESSION["data"];
+
+	foreach ( $data as $d ) {
+		echo "Nomor Surat Tugas = " . $d->reference_number;
+		echo "<br/>";
+		echo "Tanggal Pelaksanaan = " . $d->start_date . " - " . $d->end_date;
+		echo "<br/>";
+		echo "Task = " . $d->task;
+		echo "<br/>";
+		echo "Judul Laporan = " . $d->report_title;
+		echo "<br/>";
+		foreach ( $d->officers as $o ) {
+			echo $o->reference_number . "  -  " . $o->name . "  -  " . $o->officer_id ."<br/>";
+		}
+
+		echo "<br/>";
+	}
+?>
+
